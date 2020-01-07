ad2bb41f7ecaf159f7dcfecb323a8e3024857622
alifrizky <alifrzky78@gmail.com>
2020-01-07 16:12:51 +0700

    Menyelesaikan nomor 2

---
+++ Deret.java
@@ -0,0 +1,19 @@
+public class Deret{
+    public static void cetak(Object obj){
+        System.out.print(obj);
+    }
+    public static void main(String[] args){
+        String res;
+        for (int i=1; i<101; i++){
+            if(i%2 == 0 && i%3 == 0){
+                cetak(" INDONESIA ");
+            }
+            else if(i%2==0){
+                cetak(i);
+            }
+            else if(i%2==1){
+                cetak(" ganjil ");
+            }
+        }
+    }
+}
