0f387b1963d8ab990a76b64f9ef7f66661d80359
alifrizky <alifrzky78@gmail.com>
2020-01-07 16:13:39 +0700

    fixin dikit

--- Deret.java
+++ Deret.java
@@ -3,7 +3,6 @@ public class Deret{
         System.out.print(obj);
     }
     public static void main(String[] args){
-        String res;
         for (int i=1; i<101; i++){
             if(i%2 == 0 && i%3 == 0){
                 cetak(" INDONESIA ");
